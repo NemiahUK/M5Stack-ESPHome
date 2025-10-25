68cf697a47565138a77962e385eff1ac0232935e
Steven Tomlinson <steven@nemiah.uk>
2025-10-25 20:35:12 +0100

    refactor: update uhf_rfid component initialization and remove obsolete cpp file

--- components/uhf_rfid/__init__.py
+++ components/uhf_rfid/__init__.py
@@ -1,1 +1,6 @@
-# keeps ESPHome happy when filtering components: [uhf_rfid]
+# components/uhf_rfid/__init__.py
+import esphome.codegen as cg
+
+# Create the namespace and a forward-declared class symbol
+uhf_ns = cg.esphome_ns.namespace("uhf_rfid")
+UhfRfid = uhf_ns.class_("UhfRfid")  # forward-declare the type for main.cpp
--- components/uhf_rfid/text_sensor/__init__.py
+++ components/uhf_rfid/text_sensor/__init__.py
@@ -1,12 +1,13 @@
+# components/uhf_rfid/text_sensor/__init__.py
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart, text_sensor
 from esphome.const import CONF_ID, CONF_UART_ID
 
-AUTO_LOAD = ["uart"]  # it's a text_sensor platform, so no need to auto-load text_sensor
+# Import the forward-declared class from the root module
+from .. import UhfRfid
 
-uhf_ns = cg.esphome_ns.namespace("uhf_rfid")
-UhfRfid = uhf_ns.class_("UhfRfid", text_sensor.TextSensor, uart.UARTDevice, cg.Component)
+AUTO_LOAD = ["uart"]
 
 CONFIG_SCHEMA = (
     text_sensor.text_sensor_schema()
@@ -17,8 +18,12 @@ CONFIG_SCHEMA = (
 )
 
 async def to_code(config):
+    # Make sure the header is visible to the translation unit that needs the full type
+    cg.add_global(cg.RawExpression('#include "esphome/components/uhf_rfid/uhf_rfid.h"'))
+
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await text_sensor.register_text_sensor(var, config)
+
     uart_comp = await cg.get_variable(config[CONF_UART_ID])
     cg.add(var.set_uart_parent(uart_comp))
